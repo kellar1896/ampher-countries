09fbe7d366fa9394d87f31651712918fca9dcec2
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2022-04-05 15:37:57 +0300

    create header organims

--- src/components/organism/header/index.tsx
+++ src/components/organism/header/index.tsx
@@ -1,10 +1,13 @@
 import { Button } from "antd";
 import React from "react";
+import './index.css'
+import companyIcon from '../../../assets/ampher-logo.png'
 
 const Header = () => {
     return(
-        <div>
-            <Button type="primary">Hola mundo</Button>
+        <div className="header-container">
+            <img src={companyIcon} alt="" />
+            <Button type="primary">Reset Data</Button>
         </div>
     )
 }
