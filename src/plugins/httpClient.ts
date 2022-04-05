a32145c049a7deb421db2bd45eb81f51bec9ef18
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2022-04-05 15:37:00 +0300

    create a simple http client

--- src/plugins/httpClient.ts
+++ src/plugins/httpClient.ts
@@ -1,5 +1,8 @@
+import { Country } from "../types";
+
 export const HttpClient = async () => {
   const response = await fetch("https://restcountries.com/v3.1/all")
-  const body = await response.json()
+  const body = await response.json() as Country[]
   console.log(body);
+  return body
 };
